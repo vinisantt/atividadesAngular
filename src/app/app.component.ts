30bbeed0e72bcb9cd5292d520317c08e3a626089
Vinicius Dias <vdias6849@gmail.com>
2020-03-15 00:56:49 -0300

    correção de erros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,21 @@ import { Component } from "@angular/core";
 	styleUrls: ["./app.styles.css"]
 })
 export class AppComponent {
-	pessoa: string;
-	pessoas: Array<string> = [];
+	pessoa: string = "";
+	idade: number = 0;
+	pessoas: Array<any> = [];
 
-	inserirPessoa(pessoa: string) {
-		this.pessoas.push(pessoa);
+	inserirPessoa(pessoa: string, idade: number) {
+		if (pessoa == "" || idade == 0) {
+			alert("Preencha o nome e idade corretamente!");
+		} else {
+			this.pessoas.push({ nome: pessoa, idade: idade });
+			this.pessoa = "";
+			this.idade = 0;
+		}
 	}
-	excluirPessoa(p: string) {
-		let index: number = this.pessoas.indexOf(p);
+	excluirPessoa(pessoa: any) {
+		let index: number = this.pessoas.findIndex(pessoa => pessoa.nome == pessoa.nome);
 		this.pessoas.splice(index, 1);
 	}
 }
